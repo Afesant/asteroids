fae9f7f5bab340f405a837f7d9452b049f9956fc
Andres Santamaria <ingenierosantamaria@proton.me>
2025-09-15 09:01:36 -0500

    game loop

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
+from constants import *
 
+def main():
+    '''print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")'''
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill('black', rect=None, special_flags=0)
+        pygame.display.flip() 
 
 if __name__ == "__main__":
     main()
